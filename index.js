f3e87dcb954b1c61b131951c4ab28371e65c3922
Nishitha Makam <110969916+nishithamakam@users.noreply.github.com>
2023-09-09 22:40:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+
+        document.getElementById("tableBookingForm").addEventListener("submit", function(event) {
+            event.preventDefault();
+            alert("Table booked successfully!");
+        });
+         
